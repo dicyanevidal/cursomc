2a8dded657bed6c0cf37f04ffa0dc592a06c4ada
dicyanevidal <dicyanevidal@hotmail.com>
2021-07-30 18:11:01 -0300

    Criando camadas repository e service para Categoria

---
+++ src/main/java/com/dicyanevidal/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.dicyanevidal.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dicyanevidal.cursomc.domain.Categoria;
+import com.dicyanevidal.cursomc.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired  //Essa dependecia será instanciada pelo próprio spring pelo mecanismo injeção de dependencia ou inversão de controle
+	private CategoriaRepository repositorio;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repositorio.findById(id);
+		return obj.orElse(null);
+	}	
+}
